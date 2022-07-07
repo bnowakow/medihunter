3216d3934976c779c39e287dd63d30197c38d48b
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-07-07 16:56:40 +0200

    add support for multiple searches

--- run.sh
+++ run.sh
@@ -5,9 +5,14 @@ source secrets.sh
 ./env/bin/activate
 pip install --editable .
 
-# Woźny ginekolog
-python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram --start-date 2022-07-01 --end-date 2022-07-04 -i 1
+while true; do
+    echo -n `date`" - Woźny ginekolog - ";
+    #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram --start-date 2022-07-01 --end-date 2022-07-04 -i 1
+    python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram --start-date 2022-08-01
 
-# Higienistka
-#python3 medihunter.py find-appointment --specialization 112 --region 200 -n telegram --start-date 2022-06-18
-
+    # echo -n "Higienistka - "
+    # TODO filter out Telefonicznie
+    #python3 medihunter.py find-appointment --specialization 112 --region 200 -n telegram
+    
+    sleep 60;
+done
