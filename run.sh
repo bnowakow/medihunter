0b017d9e7d013710cd62bab7cccb6724c4e0593a
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-08-14 11:58:05 +0200

    sometimes -i option doesn't return new finds, making restart app after an hour timeout

--- run.sh
+++ run.sh
@@ -8,7 +8,12 @@ pip install --editable .
 while true; do
     echo -n `date`" - Woźny ginekolog - ";
     #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram --start-date 2022-07-01 --end-date 2022-07-04 -i 1
-    python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram --start-date 2022-08-01
+    # sometimes -i option doesn't return new finds, making restart app after an hour timeout
+    timeout 7200 python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram -i 1 # todo remove -i 1
+    #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram
+
+    #echo -n `date`" - Endokrynolog - ";
+    #python3 medihunter.py find-appointment --region 200 --specialization 5 -n telegram
 
     # echo -n "Higienistka - "
     # TODO filter out Telefonicznie
