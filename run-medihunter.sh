e945b028c369b652223e01f60c336376583243bd
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-08-29 16:11:24 +0200

    add kill script since ctrl-c doesn't work, so ctrl-z can be used and then kill script

--- run-medihunter.sh
+++ run-medihunter.sh
@@ -9,7 +9,7 @@ while true; do
     echo -n `date`" - Woźny ginekolog - ";
     #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram --start-date 2022-07-01 --end-date 2022-07-04 -i 1
     # sometimes -i option doesn't return new finds, making restart app after an hour timeout
-    timeout 7200 python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram -i 1 # todo remove -i 1
+    timeout 7200 python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 --start-date 2022-10-08  -n telegram -i 1 
     #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram
 
     #echo -n `date`" - Endokrynolog - ";
@@ -18,6 +18,9 @@ while true; do
     # echo -n "Higienistka - "
     # TODO filter out Telefonicznie
     #python3 medihunter.py find-appointment --specialization 112 --region 200 -n telegram
+
+    echo -n "Neurolog - "
+    python3 medihunter.py find-appointment --specialization 16 --region 200 --start-date 2022-08-28 --end-date 2022-09-21 -n telegram
     
     sleep 60;
 done
