e68b2c3de2313d3ca009920c5e8f45cdaa6a017b
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-10-21 07:37:19 +0200

    add new search

--- run-medihunter.sh
+++ run-medihunter.sh
@@ -9,7 +9,7 @@ while true; do
     echo -n `date`" - Woźny ginekolog - ";
     #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram --start-date 2022-07-01 --end-date 2022-07-04 -i 1
     # sometimes -i option doesn't return new finds, making restart app after an hour timeout
-    timeout 7200 python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 --start-date 2022-10-08  -n telegram -i 1 
+    timeout 7200 python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram -i 1 
     #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram
 
     #echo -n `date`" - Endokrynolog - ";
@@ -22,5 +22,8 @@ while true; do
     #echo -n "Neurolog - "
     #python3 medihunter.py find-appointment --specialization 16 --region 200 --start-date 2022-08-28 --end-date 2022-09-21 -n telegram
     
+    echo -n "Dermatolog Cichowska- "
+    python3 medihunter.py find-appointment --specialization 3 --doctor 417732 --region 200 --start-date 2022-11-03 --end-date 2022-11-12 -n telegram 
+
     sleep 60;
 done
