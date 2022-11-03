b67c6b70e187b355a66359d3b59c2a378a97cb00
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-11-03 17:46:28 +0100

    sloppy workaround of running few searches at once

--- run-medihunter.sh
+++ run-medihunter.sh
@@ -5,12 +5,13 @@ source secrets.sh
 ./env/bin/activate
 pip install --editable .
 
+sleep_time=7200;
+i=0;
+
 while true; do
-    echo -n `date`" - Woźny ginekolog - ";
-    #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram --start-date 2022-07-01 --end-date 2022-07-04 -i 1
+    echo -n `date`" iteration=$i - Woźny ginekolog - ";
     # sometimes -i option doesn't return new finds, making restart app after an hour timeout
-    timeout 7200 python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram -i 1 
-    #python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram
+    timeout $sleep_time python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 -n telegram -i 1 &
 
     #echo -n `date`" - Endokrynolog - ";
     #python3 medihunter.py find-appointment --region 200 --specialization 5 -n telegram
@@ -22,8 +23,10 @@ while true; do
     #echo -n "Neurolog - "
     #python3 medihunter.py find-appointment --specialization 16 --region 200 --start-date 2022-08-28 --end-date 2022-09-21 -n telegram
     
-    echo -n "Dermatolog Cichowska- "
-    python3 medihunter.py find-appointment --specialization 3 --doctor 417732 --region 200 --start-date 2022-11-03 --end-date 2022-11-12 --disable-phone-search -n telegram 
+    echo -n "Dermatolog Cichowska - ";
+    timeout $sleep_time python3 medihunter.py find-appointment --specialization 3 --doctor 417732 --region 200 --start-date 2022-11-03 --end-date 2022-11-12 --disable-phone-search -n telegram -i 1 &
 
     sleep 60;
+    sleep $sleep_time;
+    let i=$i+1;
 done
