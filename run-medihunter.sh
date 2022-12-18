3e652c0545d86dacbbf6f30a9c63a3334e062a8c
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-12-18 16:57:46 +0100

    check if date is past search query

--- run-medihunter.sh
+++ run-medihunter.sh
@@ -12,12 +12,22 @@ sleep_time=7200;
 i=0;
 
 while true; do
+    date_from=$(date +"%Y-%m-%d");
+
+    echo $date_from;
+
     #echo -n `date`" iteration=$i - Woźny ginekolog -prowadzenie -  ";
     # sometimes -i option doesn't return new finds, making restart app after an hour timeout
     #timeout $sleep_time python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 --start-date 2022-11-27 --end-date 2022-12-07 -n telegram -i 1 &
 
-    #echo -n "Woźny ginekolog  -zwykla - ";
-    #timeout $sleep_time python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4798 --disable-phone-search -n telegram -i 1 &
+    if [ "$date_from" = "2023-01-13" ]; then
+        echo "date past checkpoint";
+        exit # if that's only active search we can kill whole script
+    else
+        # TODO write code that checks if date_from is >= 2023-01-13 then start moving --start-date to today
+        echo -n "Woźny ginekolog  -zwykla - ";
+        timeout $sleep_time python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4798 --disable-phone-search --start-date 2023-01-13 --end-date 2023-01-18 -n telegram -i 1 &
+    fi
 
     #echo -n `date`" - Endokrynolog - ";
     #python3 medihunter.py find-appointment --region 200 --specialization 5 -n telegram
@@ -32,11 +42,10 @@ while true; do
     #echo -n "Dermatolog Cichowska - ";
     #timeout $sleep_time python3 medihunter.py find-appointment --specialization 3 --doctor 417732 --region 200 --start-date 2022-11-03 --end-date 2022-11-12 --disable-phone-search -n telegram -i 1 &
 
-    date_from=$(date +"%Y-%m-%d");
     #date_from=$(date +"%Y-%m-%d" --date="2 day");
-    echo "Pediatra dzieci zdrowe - ";
-    echo -n "$date_from";
-    timeout $sleep_time python3 medihunter.py find-appointment --region 200 --specialization 132 --start-date $date_from --end-date 2022-12-31 --disable-phone-search -n telegram -i 1 &
+#    echo "Pediatra dzieci zdrowe - ";
+#    echo -n "$date_from";
+#    timeout $sleep_time python3 medihunter.py find-appointment --region 200 --specialization 132 --start-date $date_from --end-date 2022-12-31 --disable-phone-search -n telegram -i 1 &
 
     sleep 60;
     sleep $sleep_time;
