1c4318e73544f41d268394b27d745b311070065e
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-06-18 01:28:17 -0700

    change vistits

--- run-medihunter.sh
+++ run-medihunter.sh
@@ -23,8 +23,8 @@ while true; do
         # sometimes -i option doesn't return new finds, making restart app after an hour timeout
         #timeout $sleep_time python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4800 --start-date 2022-11-27 --end-date 2022-12-07 -n telegram -i 1 &
 
-        #echo -n "Woźny ginekolog  -zwykla - ";
-        #timeout $sleep_time python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4798 --disable-phone-search -n telegram -i 1 &
+        echo -n "Woźny ginekolog  -zwykla - ";
+        timeout $sleep_time python3 medihunter.py find-appointment --doctor 325232 --region 200 --specialization 4798 --disable-phone-search -n telegram -i 1 &
 
         #date_start="2023-01-13";
         #date_end="2023-01-18";
@@ -49,8 +49,8 @@ while true; do
         # TODO filter out Telefonicznie
         #python3 medihunter.py find-appointment --specialization 112 --region 200 -n telegram
 
-        echo -n "Neurolog - Grunwaldzka - "
-        timeout $sleep_time python3 medihunter.py find-appointment --specialization 16 --region 200 --clinic 21950 --disable-phone-search -n telegram -i 1 &
+        #echo -n "Neurolog - Grunwaldzka - "
+        #timeout $sleep_time python3 medihunter.py find-appointment --specialization 16 --region 200 --clinic 21950 --disable-phone-search -n telegram -i 1 &
     
         #echo -n "Dermatolog Cichowska - ";
         #timeout $sleep_time python3 medihunter.py find-appointment --specialization 3 --doctor 417732 --region 200 --start-date 2022-11-03 --end-date 2022-11-12 --disable-phone-search -n telegram -i 1 &
